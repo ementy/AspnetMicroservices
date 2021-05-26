197583a964e70100d3301105014b2a5e512ecd3c
mariyan.t <mariyan.todorov13@gmail.com>
2021-05-26 12:58:30 +0300

    added discount.api to the docker-compose, migrate and seed discountdb on startup

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var servicesCollection = scope.ServiceProvider;
+                var configuration = servicesCollection.GetRequiredService<IConfiguration>();
+                var logger = servicesCollection.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postresql database.");
+
+                    var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+                    var connection = new NpgsqlConnection(connectionString);
+                    connection.Open();
+
+                    using var command = new NpgsqlCommand
+                    {
+                        Connection = connection
+                    };
+
+                    command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                    command.ExecuteNonQuery();
+
+                    //Create
+                    command.CommandText = @"CREATE TABLE Coupon(
+                                            	                ID SERIAL PRIMARY KEY NOT NULL,
+                                            	                ProductName VARCHAR(24) NOT NULL,
+                                            	                Description TEXT,
+                                            	                Amount INT);";
+                    command.ExecuteNonQuery();
+
+                    //Seed
+                    command.CommandText = "INSERT INTO Coupon (ProductName, Description, Amount) VALUES ('IPhone X', 'IPhone Discount', 150)";
+                    command.ExecuteNonQuery();
+
+                    command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100)";
+                    command.ExecuteNonQuery();
+
+                    logger.LogInformation("Migrated postresql database.");
+                }
+                catch (NpgsqlException ex)
+                {
+                    logger.LogError(ex, "An error occured while migrating the postresql database.");
+
+                    if (retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, retryForAvailability);
+                    }
+                }
+
+                return host;
+            }
+        }
+    }
+}
